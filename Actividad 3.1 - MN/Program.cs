50f62b81d4d541f70a63fbb54ef50d25e61741d9
javierctolosa <74168368+javierctolosa@users.noreply.github.com>
2021-11-02 21:41:01 -0600

    Added files via upload

---
+++ Actividad 3.1 - MN/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Actividad_3._1___MN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido(a), este programa te asiste en multiplicar una matriz por un número escalar");//Mensaje para el usuario
+            int renglones, columnas;
+            Console.Write("¿Cuántos renglones tiene tu matriz?\t");
+            renglones = int.Parse(Console.ReadLine());
+            Console.Write("¿Cuántas columnas tiene tu matriz?\t");
+            columnas = int.Parse(Console.ReadLine());
+            int[,] matriz = new int[columnas, renglones];
+            //For para llenar el arreglo
+            for (int i = 0; i < columnas; i++)
+            {
+                for (int j = 0; j < renglones; j++)
+                {
+                    Console.Write("Ingresa el número en la posición [" + (i + 1) + "][" + (j + 1) + "] de la matriz: ");//Pedimos el valor de cada una de los números en base a sus posiciones en la matriz
+                    matriz[i, j] = int.Parse(Console.ReadLine());//Almacena el valor a la matriz
+                }//Fin de for j
+            }//Fin for i
+            //Fin de for para llenar la matriz -Inicio de for para imprimir la matriz
+            for (int i = 0; i < columnas; i++)
+            {
+                for (int j = 0; j < renglones; j++)
+                {
+                    Console.WriteLine("El valor de [" + (i + 1) + "][" + (j + 1) + "] es: " + matriz[i, j]);//Imprmime el valor de la matriz en su posición
+                }//Fin de for j
+            }//Fin for i
+            Console.Write("Ingresa el valor por el que deseas multiplicar la matriz: ");//Instrucción para el usuario
+            int multiplo = int.Parse(Console.ReadLine());//Ingresamos Valor            
+            for (int i = 0; i < columnas; i++)
+            {
+                for (int j = 0; j < renglones; j++)
+                {
+                    int multiplica = 0;//Asignamos el valor
+                    multiplica = matriz[i, j] * multiplo;//Hacemos operación
+                    Console.WriteLine("El nuevo valor de [" + (i + 1) + "][" + (j + 1) + "] es: " + multiplica);//Imprime el valor de la matriz en su posición
+                }//Fin de for j
+            }//Fin for i
+            Console.Write("Gracias por utilizar este programa, ojalá te haya sido de utilidad.");
+            Console.ReadLine();
+        }
+    }
+}
+
+        
+    
+
---
+++ Actividad 3.2 - MN/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Actividad_3._2___MN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido(a), este programa te asistirá en obtener el promedio de las calificaciones de tu semestre.");
+
+            double promedio, suma = 0;//Declaramos variables
+            Console.Write("¿Cuantas materias cursas o has cursado durante el semestre?  ");
+            int cuantas = int.Parse(Console.ReadLine());//Almacenamos el número ingresado por el usuario
+            int[] calificaciones = new int[cuantas];//Declaramos arreglo y asignamos tamaño
+            for (int i = 0; i < cuantas; i++)
+            {
+                Console.Write("Ingresa tu calificación de la materia " + (i + 1) + ": ");
+                calificaciones[i] = int.Parse(Console.ReadLine());//Almacenamos el número ingresado por el usuario
+            }
+            for (int j = 0; j < cuantas; j++)
+            {
+                suma = suma + calificaciones[j];//hacemos que suma sume su valor anterior y el valor actual del arreglo
+            }
+            promedio = suma / cuantas;//Hacemos la operación de promedio
+            Console.WriteLine("Tu promedio del semestre es: " + promedio);//Imprimimos el valor del Promedio
+            if (promedio < 70)
+            {//Si su promedio es menor a 70
+                Console.WriteLine("Estas reprobando o has reprobado algunas materias en el semestre.");//Mensaje para el usuario
+            }
+            else if (promedio < 85)
+            {//si su promedio es menor a 85
+                Console.WriteLine("Seguro has aprobado una parte de tus materias, sé que puedes mejorar ese promedio.");//Mensaje para el usuario
+            }
+            else if (promedio < 95)
+            {//si su promedio es menor a 95
+                Console.WriteLine("Eres un excelente estudiante.");//Mensaje para el usuario
+            }
+            else
+            {//Si no se cumplió ninguna anterior
+                Console.WriteLine("Eres un estudiante excepcional ¡FELICIDADES!");//Mensaje para el usuario
+            }
+            Console.Write("Gracias por utilizar este programa, ojalá te haya sido de ayuda.");
+            Console.ReadLine();
+        }
+    }
+}
