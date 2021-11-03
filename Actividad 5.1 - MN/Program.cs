7235fd407cb782d17ec4a8e0c97165b74a8c0552
javierctolosa <74168368+javierctolosa@users.noreply.github.com>
2021-11-02 21:37:35 -0600

    Added files via upload

---
+++ Actividad 5.1 - MN/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Actividad_5_Programa_1_MN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                double paso = 0.01, sol_1, sol_2 = 0;
+
+                for (double i = -10; i <= 10; i = i + paso)
+                {
+                    sol_1 = Math.Pow(Math.Acos(i / 5), 2) - Math.Sin(i / 10);
+
+                    if (sol_1 * sol_2 < 0)
+                    {
+                        Console.WriteLine("La solución es: " + i);
+                    }
+
+                    sol_2 = sol_1;
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
